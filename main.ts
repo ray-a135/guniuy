a86ff6176187a6eca633fe4c26339590fc5de2e1
ray-a135 <rachelauguste36@gmail.com>
2025-04-15 13:26:34 -0700

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,42 +1,55 @@
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    projectile = sprites.createProjectileFromSprite(assets.image`bullet bill`, mySprite, 50, 50)
-    mySprite10 = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, SpriteKind.Player)
+    sprite30 = sprites.create(assets.image`bullet bill`, SpriteKind.Projectile)
+    projectile = sprites.createProjectileFromSprite(assets.image`bullet bill`, mySprite8, 50, 50)
+    projectile = sprites.createProjectileFromSprite(assets.image`bullet bill`, sprite10, 50, 50)
+    if (sprite30.overlapsWith(mySprite6)) {
+        info.changeLifeBy(1)
+        info.changeScoreBy(1)
+        enemylives += -1
+    }
+    if (sprite30.overlapsWith(mySprite5)) {
+        info.changeLifeBy(1)
+        enemylives += -1
+        info.changeScoreBy(1)
+    }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.image`juce wlrd`, function (sprite, location) {
     mySprite7 = sprites.create(assets.image`jgjeijjejejeijdijjjjjjjuejdjndjdfskfjnawjndjwndndwjdnfnjndjnnjdjcn`, SpriteKind.Player)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    sprite31 = sprites.create(assets.image`koi`, SpriteKind.Projectile)
     projectile = sprites.createProjectileFromSprite(assets.image`koi`, mySprite, 50, 50)
-    projectile = sprites.createProjectileFromSprite(assets.image`koi`, mySprite, 50, 50)
+    if (sprite31.overlapsWith(mySprite5)) {
+        info.changeLifeBy(2)
+        enemylives += -1
+        info.changeScoreBy(1)
+    }
+    if (sprite31.overlapsWith(mySprite6)) {
+        enemylives += -1
+        info.changeLifeBy(1)
+        info.changeScoreBy(1)
+    }
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
+	
+})
+let sprite31: Sprite = null
 let mySprite7: Sprite = null
-let mySprite10: Sprite = null
 let projectile: Sprite = null
+let sprite30: Sprite = null
+let sprite10: Sprite = null
+let mySprite8: Sprite = null
+let mySprite6: Sprite = null
 let mySprite: Sprite = null
+let mySprite5: Sprite = null
 info.setLife(9)
 scene.setBackgroundImage(assets.image`jgughjhhsbxsjhbacjbchjsbksxh0iduihsdf`)
 tiles.setCurrentTilemap(tilemap`level1`)
 tiles.setCurrentTilemap(tilemap`level3`)
-let mySprite5 = sprites.create(assets.image`ivo`, SpriteKind.Enemy)
+info.setScore(0)
+mySprite5 = sprites.create(assets.image`ivo`, SpriteKind.Enemy)
 mySprite = sprites.create(assets.image`snopp dogg`, SpriteKind.Player)
-let mySprite6 = sprites.create(img`
+mySprite6 = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     f f f . . . . . . . f f f . . . 
@@ -54,8 +67,8 @@ let mySprite6 = sprites.create(img`
     f f . . . f f d f d f d . . . . 
     . . . . f f f f f f f f . . . . 
     `, SpriteKind.Enemy)
-let mySprite8 = sprites.create(assets.image`oip`, SpriteKind.Player)
-let sprite10 = sprites.create(assets.image`oipo`, SpriteKind.Player)
+mySprite8 = sprites.create(assets.image`oip`, SpriteKind.Player)
+sprite10 = sprites.create(assets.image`oipo`, SpriteKind.Player)
 let mySprite9 = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -106,6 +119,8 @@ controller.moveSprite(mySprite)
 mySprite5.setPosition(22, 28)
 mySprite6.setPosition(34, 42)
 scene.cameraFollowSprite(mySprite)
+info.setLife(25)
+let enemylives = 22
 forever(function () {
 	
 })
